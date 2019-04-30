3584a1a4e5fa154d1e6003caa4cf3a677db10955
SwetaMisra9 <misrasweta2@gmail.com>
2019-04-30 07:52:55 -0700

    Build reports added

---
+++ ProjectManager_Server/.vs/ProjectManager/lut/9244/1/ProjectManager.Tests/bin/Debug/coverage.bat
@@ -0,0 +1,2 @@
+
+C:\Users\Administrator\Desktop\FSE-ProjectManager\ProjectManager_Server\packages\NUnit.ConsoleRunner.3.10.0\tools\nunit3-console.exe C:\Users\Administrator\Desktop\FSE-ProjectManager\ProjectManager_Server\ProjectManager.Tests\bin\Debug\ProjectManager.Tests.dll
